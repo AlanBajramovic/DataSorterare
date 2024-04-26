7276704b38e5cad7be6fff8aca7e6840cdd609ce
anxy <143069429+AlanBajramovic@users.noreply.github.com>
2024-04-26 10:51:21 +0200

    Update data-sort.py
    Commit 5

--- data-sort.py
+++ data-sort.py
@@ -1,64 +1,43 @@
-import PySimpleGUI as sg #Importering av PySimpleGUI och sg blev använt som förkortning 
+import PySimpleGUI as sg  
 import subprocess
 import os
 
-def main():
-    subprocess.run([os.sys.excutable, 'quick-sort.py'])
+def run_script(script_name):
+    subprocess.run([os.sys.executable, script_name])
 
 def create_window(theme):
-    sg.theme(theme) #Ange tema 
-    sg.set_options(font = 'Arial 25', button_element_size = (2,2))
-    button_size = (2,2) # Storlek på knappar 
+    sg.theme(theme) 
+    sg.set_options(font = 'Arial 25', button_element_size = (2,2)) 
+
  
     layout = [
         [sg.Text(
-            '!-DATA SORT-!', #Text som visas i fönstret 
+            '!-DATA SORT-!',
             font = 'Arial 25', 
             justification= 'center', 
             expand_x = True, 
             pad = (2,2),
-            key = '-TEXT-') #Exempel på nyckel som används för att identifera texen 
+            right_click_menu = 
+            ['Menu', ['Lightgrey1', 'SandyBeach', 'Tan', 'Random']])
         ],
-        [
-        sg.Button('GUIDE', expand_x = True), 
-        sg.Button('MERGE SORT ', expand_x = True), 
-        sg.Button('BUBBLE SORT', expand_x = True), 
-        sg.Button('INSERTION SORT', expand_x = True), 
-        sg.Button('QUICK SORT', expand_x = True)
-        ],
-    ]
+        [sg.Button('Meny', expand_x = True), 
+         sg.Button('Merge Sort ', expand_x = True), 
+         sg.Button('Bubble Sort', expand_x = True), 
+         sg.Button('Insertion Sort', expand_x = True), 
+         sg.Button('Quick Sort', expand_x = True)],
+        ]
     
-    return sg.Window('data-sort.py', layout)
-#Temamenyn 
-theme_menu = ['Menu', ['Blue']]
-window = create_window('DarkBlue')
-#Skapa en fönster med tema dark
+    return sg.Window('Guide', layout)
 
-current_num = []
-full_operation = []
+
+window = create_window('dark')
 
 while True: 
     event, values = window.read()
     if event == sg.WIN_CLOSED:
         break
-    
-    if event in theme_menu[1]:
-        window.close()
-        window = create_window(event)
 
-    if event == ('GUIDE'):
-        print('GUIDE')
-
-    if event == ('MERGE SORT'):
-        print('MERGE SORT')
-
-    if event == ('BUBBLE SORT'):
-        print('BUBBLE SORT')
+    if event == ('Data Sort'):
+        run_script('data-sort.py')
         
-    if event == ('INSERTION SORT'):
-        print('INSERTION SORT')
-    
-    if event == ('QUICK SORT'):
-        print('QUICK SORT')
-        
-window.close() #Stänger ner fönstret (miniräknaren) när loopen är klar
+window.close() 
