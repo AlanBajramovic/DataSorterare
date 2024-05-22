4fd0cc42400111316a6477af557bebac235214c9
anxy <143069429+AlanBajramovic@users.noreply.github.com>
2024-05-22 13:38:10 +0200

    Update bubble-sort.py

--- bubble-sort.py
+++ bubble-sort.py
@@ -2,9 +2,11 @@ import PySimpleGUI as sg
 import subprocess
 import os
 
+# Funktion för att köra andra Python-skript
 def run_script(script_name):
     subprocess.run([os.sys.executable, script_name])
 
+# Bubble Sort-algoritm
 def bubble_sort(numbers):
     n = len(numbers)
     for i in range(n):
@@ -13,10 +15,12 @@ def bubble_sort(numbers):
                 numbers[j], numbers[j+1] = numbers[j+1], numbers[j]
     return numbers
 
+# Skapa Bubble Sort-fönstret
 def create_window(theme):
     sg.theme(theme) 
     sg.set_options(font='Arial 25', button_element_size=(2, 2)) 
 
+    # Layouten för Bubble Sort-fönstret
     layout = [
         [sg.Text(
             '!-BUBBLE SORT-!',
@@ -39,11 +43,13 @@ def create_window(theme):
 
 window = create_window('SandyBeach1')
 
+# Huvudloop för att läsa händelser i fönstret
 while True: 
     event, values = window.read()
-    if event == sg.WIN_CLOSED:
+    if event == sg.WIN_CLOSED: # Stäng fönstret
         break
 
+    # Hantera knapparna Meny, Guide och Sortera
     if event == 'Meny':
         window.close()
         run_script('meny.py')
@@ -55,10 +61,14 @@ while True:
     if event == 'Sortera':
         input_numbers = values['input_numbers']
         try:
+            # Konvertera inmatade nummer till en lista av heltal
             numbers = list(map(int, input_numbers.split(',')))
+            # Sorterar nummer med Bubble Sort    
             sorted_numbers = bubble_sort(numbers)
+            # Visar resultat i output-fältet
             window['output'].update(', '.join(map(str, sorted_numbers)))
         except ValueError:
+             # Hanterar det som blir fel 
             window['output'].update('Felaktig inmatning, ange endast nummer separerade med kommatecken.')
 
 window.close()
