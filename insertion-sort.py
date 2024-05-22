98fb9228def8578517288d4012a893eced2e2602
anxy <143069429+AlanBajramovic@users.noreply.github.com>
2024-05-22 13:37:37 +0200

    Update insertion-sort.py

--- insertion-sort.py
+++ insertion-sort.py
@@ -2,9 +2,11 @@ import PySimpleGUI as sg
 import subprocess
 import os
 
+# Funktion för att köra andra Python-skript
 def run_script(script_name):
     subprocess.run([os.sys.executable, script_name])
 
+# Insertion Sort-algoritm
 def insertion_sort(numbers):
     for i in range(1, len(numbers)):
         key = numbers[i]
@@ -15,6 +17,7 @@ def insertion_sort(numbers):
         numbers[j + 1] = key
     return numbers
 
+# Skapa Insertion Sort-fönstret
 def create_window(theme):
     sg.theme(theme) 
     sg.set_options(font='Arial 25', button_element_size=(2, 2))
@@ -40,12 +43,12 @@ def create_window(theme):
     return sg.Window('Insertion Sort', layout)
 
 window = create_window('SandyBeach1')
-
+# Huvudloop för att läsa händelser i fönstret
 while True: 
     event, values = window.read()
-    if event == sg.WIN_CLOSED:
+    if event == sg.WIN_CLOSED: # Stäng fönstret
         break
-
+# Hantera knapparna Meny, Guide och Sortera
     if event == 'Meny':
         window.close()
         run_script('meny.py')
@@ -57,10 +60,14 @@ while True:
     if event == 'Sortera':
         input_numbers = values['input_numbers']
         try:
+            # Konvertera inmatade nummer till en lista av heltal
             numbers = list(map(int, input_numbers.split(',')))
+            # Sortera nummer med Insertion Sort
             sorted_numbers = insertion_sort(numbers)
+            # Visar upp resultat i output-fältet
             window['output'].update(', '.join(map(str, sorted_numbers)))
         except ValueError:
+            # Hanterar det som var fel inskriven 
             window['output'].update('Felaktig inmatning, ange endast nummer separerade med kommatecken.')
 
 window.close()
